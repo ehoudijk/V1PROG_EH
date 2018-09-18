c2d2acb7ef8c6e816ab9468bfa6bb8d631846a4f
ehoudijk <eric.houdijk@student.hu.nl>
2018-09-18 16:14:42 +0200

    eerste installatie GIT

---
+++ Les6/pe6_1.py
@@ -0,0 +1,9 @@
+def som(x, y, z):
+    return x + y + z
+
+
+getal1 = 1
+getal2 = 2
+getal3 = 3
+
+print(som(getal1, getal2, getal3))
---
+++ Oefenen/Test.py
@@ -0,0 +1,8 @@
+def som(x, y, z):
+    return x + y + z
+
+x = 1
+y = 2
+z = 3
+
+print(som(x, y, z))
