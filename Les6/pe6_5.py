ad24bbd99bde253ce7469584a5356d96bcdca3ff
ehoudijk <eric.houdijk@student.hu.nl>
2018-09-18 21:22:59 +0200

    update

--- Les6/pe6_5.py
+++ Les6/pe6_5.py
@@ -1,1 +1,7 @@
 def kwadraten_som(grondgetallen):
+    for i in grondgetallen:
+        return [i**2 for i in grondgetallen if i > 0]
+
+
+lijst = [4, 5, 3, -81]
+print(kwadraten_som(lijst))
--- Oefenen/Test.py
+++ Oefenen/Test.py
@@ -1,16 +1,11 @@
-import re
+def wijzig(letterlijst):
+    letterlijst.clear()
+    letterlijst.append('d')
+    letterlijst.append('e')
+    letterlijst.append('f')
 
-def new_password(oldpassword, newpassword):
-    if len(newpassword) < 6:
-        print('Het nieuwe wachtwoord moet minstens 6 karakters bevatten waarvan minimaal 1 cijfer.')
-    elif newpassword == oldpassword:
-        print('Het nieuwe wachtwoord mag niet gelijk zijn aan het oude wachtwoord.')
-    elif re.search('[0-9]', newpassword) is None:
-        print('Het nieuwe wachtwoord moet een cijfer bevatten.')
-    else:
-        print('Het nieuwe wachtwoord is geaccepteerd.')
 
-oud = 'test1234'
-nieuw = input('Geef een nieuw wachtwoord: ')
-
-new_password(oud, nieuw)
+lijst = ['a', 'b', 'c']
+print(lijst)
+wijzig(lijst)
+print(lijst)
